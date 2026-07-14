be50be048a340449b1c722d765d641d49bb5f382
Arghya Mukherjee <arghyamukherjee20@outlook.com>
2026-07-14 14:36:22 +0530

    Add comment

--- Auth.py
+++ Auth.py
@@ -20,23 +20,19 @@ oauth.register(
 # print("Google Client ID:", os.getenv('Google_Client_ID'))
 # print("Google Client Secret:", os.getenv('Google_Client_Secret'))
 
+## Login route
 @app.route('/login')
 def login():
     redirect_uri = url_for('auth', _external=True)
   #  print("Client ID:", os.getenv("Google_Client_ID"))
     print("Redirect URI:", url_for('auth', _external=True))
     return oauth.google.authorize_redirect(redirect_uri)
-  
 
+## Auth route
 @app.route('/auth')
 def auth():
     token = oauth.google.authorize_access_token()
     return jsonify(token['userinfo'])
-
-
-
-
-
 
 if __name__ == '__main__':
     app.run(debug=True)
